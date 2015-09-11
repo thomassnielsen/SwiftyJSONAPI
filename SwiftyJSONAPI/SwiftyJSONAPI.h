9883180dfa553d519057fea6c580dc4c2ee87afc
thomassnielsen <me@thomassnielsen.com>
2015-09-11 11:19:08 +0200

    Initial Commit

---
+++ SwiftyJSONAPI/SwiftyJSONAPI.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyJSONAPI.h
+//  SwiftyJSONAPI
+//
+//  Created by Thomas Sunde Nielsen on 11.09.15.
+//  Copyright © 2015 Thomas Sunde Nielsen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftyJSONAPI.
+FOUNDATION_EXPORT double SwiftyJSONAPIVersionNumber;
+
+//! Project version string for SwiftyJSONAPI.
+FOUNDATION_EXPORT const unsigned char SwiftyJSONAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyJSONAPI/PublicHeader.h>
+
+
